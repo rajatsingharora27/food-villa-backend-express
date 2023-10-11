65adf2cdb39ac7177f1590eda6852ff8b44e8445
rajat arora <rajatarora@rajats-MacBook-Air.local>
2023-10-12 00:40:28 +0530

    feature_rajat

--- src/service/AddProductService.js
+++ src/service/AddProductService.js
@@ -6,12 +6,16 @@ class AddProductService {
     // const isValidName =this.#validateName(productData.productName);
     // const isValidProductDetails=this.#validateProductDetails(productData.productDetails);
     let errorList=[]
+    console.log("hello")
     const [isValidName,isValidProductDetails]=await Promise.all([
       this.#validateName(productData.productName,errorList),
+      
       this.#validateProductDetails(productData.productDetails,errorList),
     ]);
 
     // check if the procuct already exist in the database? ->on product name
+
+
 
     //save in database if passed
 
